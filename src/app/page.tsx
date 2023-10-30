623a0a339e885703ad326622d8881450ce71c68d
adrianczekaj <aczekaj.inf@gmail.com>
2023-10-30 20:19:22 +0100

    L3 - antd demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from 'antd'
+
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <Button type="primary">Antd button</Button>
     </div>
   )
 }
