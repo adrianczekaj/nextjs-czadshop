ed07e41ca97695c7c21ce19e171a8f0b2d0ff454
adrianczekaj <aczekaj.inf@gmail.com>
2023-10-30 20:55:34 +0100

    L5 - register page 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from 'antd'
-
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
-      <Button type="primary">Antd button</Button>
     </div>
   )
 }
