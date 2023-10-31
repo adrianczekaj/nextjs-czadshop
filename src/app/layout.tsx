31a2e2af91c0e10008afbbb7f9e632162c9cbeee
adrianczekaj <aczekaj.inf@gmail.com>
2023-10-31 08:17:50 +0100

    L6 - register page 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import type { Metadata } from 'next'
 import './globals.css'
-
+import ThemeProvider from "@/providers/ThemeProvider";
 
 export const metadata: Metadata = {
   title: 'Czadshop-Next Dev',
@@ -14,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
