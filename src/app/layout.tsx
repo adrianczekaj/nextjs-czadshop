351e78527150043f6d37f536ab87bc5d89180f03
adrianczekaj <aczekaj.inf@gmail.com>
2023-10-31 13:24:17 +0100

    L17-18 - create basic layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,26 @@
 import React from "react";
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 import ThemeProvider from "@/providers/ThemeProvider";
+import LayoutProvider from "@/providers/LayoutProvider";
 
 export const metadata: Metadata = {
-  title: 'Czadshop-Next Dev',
-  description: 'An e-commerce site built with Next.js and TypeScript',
-}
+  title: "Czadshop-Next Dev",
+  description: "An e-commerce site built with Next.js and TypeScript",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <ThemeProvider>
-          {children}
+          <LayoutProvider>{children}</LayoutProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
---
+++ src/providers/LayoutProvider.tsx
@@ -0,0 +1,85 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { App, Popover } from "antd";
+
+function LayoutProvider({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { message } = App.useApp();
+  const [currentUser, setCurrentUser] = useState<any>(null);
+  const pathname = usePathname();
+  const isPrivatePage =
+    pathname !== "/auth/login" && pathname !== "/auth/register";
+
+  const getCurrentUser = async () => {
+    try {
+      const response = await axios.get("/api/auth/currentuser");
+      setCurrentUser(response.data.data);
+    } catch (e: any) {
+      message.error(e.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  const onLogout = async () => {
+    try {
+      await axios.get("/api/auth/logout");
+      message.success("Logout succesfully");
+      setCurrentUser(null);
+      router.push("/auth/login");
+    } catch (e: any) {
+      message.error(e.response.data.message);
+    }
+  };
+
+  const content = (
+    <div className="flex flex-col gap-2 p-2">
+      <div
+        className="flex gap-2 items-center cursor-pointer text-md"
+        onClick={() => router.push("/profile")}
+      >
+        <i className="ri-shield-user-line"></i>
+        <span>Profile</span>
+      </div>
+      <div
+        className="flex gap-2 items-center cursor-pointer text-md"
+        onClick={onLogout}
+      >
+        <i className="ri-logout-box-r-line"></i>
+        <span>Log out</span>
+      </div>
+    </div>
+  );
+
+  return (
+    <div>
+      {isPrivatePage && currentUser && (
+        <>
+          <div className="bg-primary py-2 px-5 flex justify-between items-center">
+            <div className="flex gap-2">
+              <h1 className="text-2xl font-bold text-red-500">Czad</h1>
+              <h1 className="text-2xl font-bold text-yellow-500">Shop</h1>
+            </div>
+            <div className="flex gap-5 items-center">
+              <i className="ri-shopping-cart-line text-white text-2xl"></i>
+              <Popover content={content} trigger="click">
+                <div className="flex h-8 w-8 bg-white p-2 rounded-full items-center justify-center cursor-pointer">
+                  <span>{currentUser.name[0].toUpperCase()}</span>
+                </div>
+              </Popover>
+            </div>
+          </div>
+          <div className="p-5">{children}</div>
+        </>
+      )}
+
+      {!isPrivatePage && children}
+    </div>
+  );
+}
+
+export default LayoutProvider;
