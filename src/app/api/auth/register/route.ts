1c419a8ba3ff981684e86b6ea4f5ab311d96e31a
adrianczekaj <aczekaj.inf@gmail.com>
2023-10-31 10:18:40 +0100

    L11 - user register API

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectDB } from "@/configs/dbConfig";
+import User from "@/models/userModel";
+import bcrypt from "bcryptjs";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+
+    // check if user already exists
+    const userExists = await User.findOne({
+      email: reqBody.email,
+    });
+    if (userExists) {
+      throw new Error("User already exists");
+    }
+
+    //create new user
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(reqBody.password, salt);
+    reqBody.password = hashedPassword;
+    const newUser = new User(reqBody);
+    await newUser.save();
+
+    return NextResponse.json({
+      message: "User created successfully",
+      data: newUser,
+    });
+  } catch (e: any) {
+    return NextResponse.json(
+      {
+        message: e.message,
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+}
