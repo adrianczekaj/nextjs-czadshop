c4b9a566d398727e94c24972f29314de5045ee2b
adrianczekaj <aczekaj.inf@gmail.com>
2023-11-02 08:08:36 +0100

    L21-22 - set up redux and create tabs in profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import { Tabs, TabsProps } from "antd";
+import { useSelector } from "react-redux";
+
+function Profile() {
+  const { currentUser } = useSelector((state: any) => state.user);
+
+  const adminTabs: TabsProps["items"] = [
+    { key: "1", label: "Products", children: "Products" },
+    { key: "2", label: "Categories", children: "Categories" },
+    { key: "3", label: "Orders", children: "Orders" },
+    { key: "4", label: "Users", children: "Users" },
+  ];
+
+  const userTabs: TabsProps["items"] = [
+    { key: "1", label: "Orders", children: "Orders" },
+    {
+      key: "2",
+      label: "Personal information",
+      children: "Personal information",
+    },
+  ];
+
+  return (
+    <div>
+      {currentUser.isAdmin && <Tabs items={adminTabs}></Tabs>}
+      {!currentUser.isAdmin && <Tabs items={userTabs}></Tabs>}
+    </div>
+  );
+}
+
+export default Profile;
